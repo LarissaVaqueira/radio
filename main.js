ce961f645edcbe3176438f6243ef68cf0bdc80d8
Larissa <106314582+LarissaVaqueira@users.noreply.github.com>
2023-11-17 09:24:08 -0300

    radio

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play()
+}
+const listadeTeclas = document.querySelectorAll(".tecla");
+for (let contador = 0; contador < listadeTeclas.length; contador++) {
+    const tecla = listadeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    };
+}
